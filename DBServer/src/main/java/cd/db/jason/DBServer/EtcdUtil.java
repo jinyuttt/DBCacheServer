3d20b4b63715a17c2fa6fc30115f2d354bfbe476
e <jinyu20072787@sina.com>
2018-08-14 06:12:19 +0800

    扩展集群

---
+++ DBServer/src/main/java/cd/db/jason/DBServer/EtcdUtil.java
@@ -0,0 +1,75 @@
+/**    
+ * 文件名：EtcdUtil.java    
+ *    
+ * 版本信息：    
+ * 日期：2018年8月14日    
+ * Copyright 足下 Corporation 2018     
+ * 版权所有    
+ *    
+ */
+package cd.db.jason.DBServer;
+
+import java.net.URI;
+
+import mousio.client.retry.RetryOnce;
+import mousio.etcd4j.EtcdClient;
+
+/**    
+ *     
+ * 项目名称：DBServer    
+ * 类名称：EtcdUtil    
+ * 类描述：    
+ * 创建人：jinyu    
+ * 创建时间：2018年8月14日 上午5:45:15    
+ * 修改人：jinyu    
+ * 修改时间：2018年8月14日 上午5:45:15    
+ * 修改备注：    
+ * @version     
+ *     
+ */
+public class EtcdUtil {
+    //etcl客户端链接
+    private static EtcdClient client = null;
+    //链接初始化
+    public static synchronized EtcdClient getEtclClient(){
+        if(null == client){
+             client = new EtcdClient(URI.create("http://127.0.0.1:4001"));
+            client.setRetryHandler(new RetryOnce(20));
+        }
+            return client;
+    }
+
+    /**
+     * 根据指定的配置名称获取对应的value
+     * @param key 配置项
+     * @return
+     * @throws Exception
+     */
+    public static String getEtcdValueByKey(String key) throws Exception {
+      client.get(key);
+       return key;
+    }
+
+    /**
+     * 新增或者修改指定的配置
+     * @param key
+     * @param value
+     * @return
+     */
+    public static void putEtcdValueByKey(String key,String value) throws Exception{
+       client.put(key, value);
+
+    }
+
+    /**
+     * 删除指定的配置
+     * @param key
+     * @return
+     */
+    public static void deleteEtcdValueByKey(String key){
+        client.delete(key);
+    }
+
+//V3 api配置初始化和监听
+
+}
